32a02987204b27744f489993c464b94241303c42
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2020-11-30 20:12:28 +0700

    🔨: Menambahkan Aksi di Activity Awal
    Signed-off-by: Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>

--- app/src/main/java/com/example/tugasintent/Awal.kt
+++ app/src/main/java/com/example/tugasintent/Awal.kt
@@ -1,11 +1,30 @@
 package com.example.tugasintent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class Awal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_awal)
+        // Inisialisasi komponen
+        val buttonPenjelasan = findViewById<LinearLayout>(R.id.buttonPenjelasan)
+        val buttonImplementasi = findViewById<LinearLayout>(R.id.buttonImplementasi)
+        val buttonAnggota = findViewById<LinearLayout>(R.id.buttonAnggota)
+        // Aksi ketika tombol di klik
+        buttonPenjelasan.setOnClickListener {
+            intent = Intent(this, Penjelasan::class.java)
+            startActivity(intent)
+        }
+        buttonImplementasi.setOnClickListener {
+            intent = Intent(this, Form::class.java)
+            startActivity(intent)
+        }
+        buttonAnggota.setOnClickListener {
+            intent = Intent(this, AnggotaKelompok::class.java)
+            startActivity(intent)
+        }
     }
 }
