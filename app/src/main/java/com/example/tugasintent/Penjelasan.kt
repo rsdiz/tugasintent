196854a1791cd1744fb7df5d20ce58c4ec65e48d
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2020-11-30 10:12:55 +0700

    nambah halaman data form
    Signed-off-by: Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>

--- app/src/main/java/com/example/tugasintent/Penjelasan.kt
+++ app/src/main/java/com/example/tugasintent/Penjelasan.kt
@@ -1,11 +1,11 @@
 package com.example.tugasintent
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class Penjelasan : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_penjelasan)
     }
 }
