88e6836fca53a8ce70c6d2f69613db7b2520151f
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2020-11-30 18:10:16 +0700

    💄: Merubah tampilan
    Signed-off-by: Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>

--- app/src/main/java/com/example/tugasintent/Form.kt
+++ app/src/main/java/com/example/tugasintent/Form.kt
@@ -7,32 +7,32 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class activity_form : AppCompatActivity() {
-    private lateinit var etNama:EditText
-    private lateinit var etNim:EditText
-    private lateinit var etProdi:EditText
-    private lateinit var etAsal:EditText
-    private lateinit var btnSimpan:Button
+class Form : AppCompatActivity() {
+    private lateinit var etNama: EditText
+    private lateinit var etNim: EditText
+    private lateinit var etProdi: EditText
+    private lateinit var etAsal: EditText
+    private lateinit var btnSimpan: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
 
-        //INISIALISASI
+        // INISIALISASI
         etNama = findViewById(R.id.et_nama)
         etNim = findViewById(R.id.et_nim)
         etProdi = findViewById(R.id.et_prodi)
         etAsal = findViewById(R.id.et_asal)
         btnSimpan = findViewById(R.id.btn_simpan)
 
-        //AKSI KETIKA BUTTON DI KLIK
+        // AKSI KETIKA BUTTON DI KLIK
         btnSimpan.setOnClickListener {
-            //CEK EDIT TEXT KOSONG ATAU TIDAK
+            // CEK EDIT TEXT KOSONG ATAU TIDAK
             if (
-                    etNama.text.isNullOrBlank()
-                    or etNim.text.isNullOrBlank()
-                    or etProdi.text.isNullOrBlank()
-                    or etAsal.text.isNullOrBlank()
+                etNama.text.isNullOrBlank()
+                or etNim.text.isNullOrBlank()
+                or etProdi.text.isNullOrBlank()
+                or etAsal.text.isNullOrBlank()
             ) {
                 Toast.makeText(this, "DATA BELUM LENGKAP", Toast.LENGTH_SHORT).show()
             } else {
